63d64c7d441c58bfb5f5b9d30d8474ecd92dffcd
Mudassar Memon <mudassar95memon@gmail.com>
2023-07-20 16:33:12 -0400

    Create second_anagram? method

--- practice-for-ch-ruby-execution-time-differences-long-practice-main/anagrams.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/anagrams.rb
@@ -5,6 +5,20 @@ def first_anagram?(str1, str2)
 end
 # total time complexity if 0(n!)
 
-p first_anagram?("gizmo", "sally")    #=> false
-p first_anagram?("elvis", "lives")    #=> true
+# p first_anagram?("gizmo", "sally")    #=> false
+# p first_anagram?("elvis", "lives")    #=> true
 
+def second_anagram?(str1, str2)
+
+    str1.each_char do |char| #n
+        if str2.index(char).is_a?(Integer) #n
+            str2[str2.index(char)] = ""
+        else 
+            return false
+        end
+    end
+    return true if str2 == ""
+    return false    
+end
+p second_anagram?("gizmo", "sally")    #=> false
+p second_anagram?("elvis", "lives")    #=> true
