f56e8b325cd8e3cdc590b0d56b4f66cd1d826f19
ajb-4 <alexbrown4711@gmail.com>
2023-07-20 13:54:56 -0400

    done with my_min2

--- practice-for-ch-ruby-execution-time-differences-long-practice-main/time_exec_diff.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/time_exec_diff.rb
@@ -8,3 +8,16 @@ end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 p my_min(list)  # =>  -5
+
+def my_min2(arr) 
+    min = arr[0] #1
+    arr.each do |ele| #n
+        min = ele if ele < min #1
+    end
+    return min #1
+end
+#total time complexity of O(n)
+
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+p my_min2(list)  # =>  -5
+
