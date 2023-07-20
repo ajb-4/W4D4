a30b07ce7b74910871454bff73f4d0df4c80ed21
Mudassar Memon <mudassar95memon@gmail.com>
2023-07-20 15:27:29 -0400

    Complete time_exec_diff project

--- practice-for-ch-ruby-execution-time-differences-long-practice-main/time_exec_diff.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/time_exec_diff.rb
@@ -46,3 +46,16 @@ p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
 
 list = [-5, -1, -3]
 p largest_contiguous_subsum(list) # => -1 (from [-1])
+
+def largest_contiguous_subsum2(arr)
+
+    current_sum = 0
+    largest_sum = 0
+
+    arr.each do |ele| #n
+        current_sum += ele
+        largest_sum = current_sum if current_sum > largest_sum
+        current_sum = 0 if current_sum < 0
+    end
+    largest_sum 
+end
